57f942ff56fbc96590434c2a40e945ad56c82c9e
Rémi Marçais <remi.marcais87@gmail.com>
2022-03-21 17:43:47 +0100

    Fix bugs

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -11,6 +11,13 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 		node->parent->parent == NULL)
 		return (NULL);
 	if (node->parent == node->parent->parent->left)
-		return (node->parent->parent->right);
-	return (node->parent->parent->left);
+	{
+		if (node->parent->parent->right)
+			return (node->parent->parent->right);
+		else
+			return (NULL);
+	}
+	if (node->parent->parent->left)
+		return (node->parent->parent->left);
+	return (NULL);
 }
