f8cf31000de8b09a1ec90facb3e3a4ecf7ba760d
Elodie Riou <elodie.riou@hotmail.com>
2022-03-22 10:01:23 +0100

    add some prototype

--- binary_trees.h
+++ binary_trees.h
@@ -21,7 +21,9 @@ struct binary_tree_s
     struct binary_tree_s *right;
 };
 typedef struct binary_tree_s binary_tree_t;
-
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
 
 void binary_tree_print(const binary_tree_t *tree);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
@@ -45,4 +47,5 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
+int binary_tree_is_bst(const binary_tree_t *tree);
 #endif /* BINARY_TREES_H */
